cb0f71b1dc0ececf90c410df23a7a1ebcfd099aa
John C. Daub <hsoi@hsoi.com>
2016-11-13 14:38:42 -0600

    Initial Commit

---
+++ Signed Comment/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Signed Comment
+//
+//  Created by hsoi on 11/13/16.
+//  Copyright © 2016 Hsoi Enterprises LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
